27e8e0928e54ee72f60863e5e43f8447b766e689
andy mcghie <mcghiea@gmail.com>
2017-11-13 04:37:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,17 @@ function whisper(string){
   return string.toLowerCase
 }
 function logShout(string) {
-  if (string.toUpperCase() === string){
-    console.log(string.toUpperCase)
+  console.log(string.toUpperCase)
+}
+function lowWhisper(string) {
+  console.log(string.toLowerCase)
+}
+function sayHiToGrandma(string){
+  if (string.toLowerCase === string){
+    return "I can't hear you!"}
+  if(string.toUpperCase === string){
+    return "YES INDEED!"}  
+  if (string === "I love you, Grandma."){
+    return "I love you, too."
   }
 }
